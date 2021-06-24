25b05ed7ee5cfe2bc737c3bd7c14e8e7159bb0c1
Key-github <keezeden@gmail.com>
2021-06-24 23:14:13 +1200

    icon and additional styling

--- src/components/home-display/index.js
+++ src/components/home-display/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const HomeDisplay = ({ currentCalories, totalCalories }) => {
   return (
-    <div>
-      <div>
-        <p className="text-gray-500">Per Package</p>
-        <p className="text-2xl">{currentCalories} Calories</p>
+    <div className="w-full flex flex-col items-center">
+      <div className="w-2/3">
+        <p className="text-gray-500 text-left">Per Package</p>
+        <p className="text-2xl text-left">{currentCalories} Calories</p>
       </div>
-      <div>
-        <p className="text-gray-500">Total</p>
-        <p className="text-2xl">{totalCalories} Calories</p>
+      <div className="w-2/3">
+        <p className="text-gray-500 text-left">Total</p>
+        <p className="text-2xl text-left">{totalCalories} Calories</p>
       </div>
     </div>
   );
