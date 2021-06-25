b1314658d2f6ba93843eb09c340f3f8301c57e5c
Key-github <keezeden@gmail.com>
2021-06-25 23:10:46 +1200

    PWA compatibility

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import { App } from './src/app';
 
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'));
+}
+
 const root = document.getElementById('root');
 
 render(<App />, root);
--- src/components/home-inputs/index.js
+++ src/components/home-inputs/index.js
@@ -6,13 +6,13 @@ const HomeInputs = ({ updateKj, updateServings }) => {
       <input
         type="number"
         placeholder="Kj Per Serving"
-        className="w-2/3 h-16 text-center focus:outline-none border-2 rounded-lg border-primary px-4 py-2 my-2"
+        className="w-2/3 h-16 text-center focus:outline-none focus:border-primary border-2 border-gray-200 bg-gray-200 rounded-lg px-4 py-2 my-2"
         onChange={updateKj}
       />
       <input
         type="number"
         placeholder="Servings Per Package"
-        className="w-2/3 h-16 text-center focus:outline-none border-2 rounded-lg border-secondary px-4 py-2 my-2"
+        className="w-2/3 h-16 text-center focus:outline-none focus:border-secondary bg-gray-200 border-2 border-gray-200 rounded-lg px-4 py-2 my-2"
         onChange={updateServings}
       />
     </div>
